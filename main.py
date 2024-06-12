f57e150eb3dfff012a5788b618327b0210760da2
Oleg Sheinov <oleg20064@gmail.com>
2024-06-12 15:34:59 +0200

    Правки кода. Теперь анализ через ИИ

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import json
 import logging
 import os
 import re
 
 from dotenv import load_dotenv
+from g4f.client import Client
 from telethon import TelegramClient, events
 
 from api.post_vacancy import send_vacancy
@@ -16,21 +18,62 @@ log = logging.getLogger()
 client = TelegramClient(session=os.getenv("SESSION_NAME"), api_id=int(os.getenv('API_ID')),
                         api_hash=os.getenv('API_HASH'))
 all_chats = []
-my_chat = int(os.getenv('MY_CHANNEL'))
-new_tg = os.getenv('NEW_TG')
-regex_bid = ["рейт", "ставк", "цена", "р/ч", "оплат"]
-regex_tg = r'@\w*|\[[^\]]+\]\([^)]+\)'
 
 phone = os.getenv("PHONE")
 password = os.getenv("PASS")
 
 
+async def is_valid_json(text):
+    try:
+        json.loads(text)
+        return True
+    except json.JSONDecodeError:
+        return False
+
+
+async def json_to_dict(json_message):
+    if "```json" in json_message:
+        pattern = re.compile(r'```json(.*?)```', re.DOTALL)
+        matches = pattern.findall(json_message)
+        json_objects = [
+            json.loads(match.strip())
+            for match in matches
+            if await is_valid_json(match.strip())
+        ]
+
+        return json_objects[0]
+    else:
+        return json.loads(json_message.replace("\n", ""))
+
+
+def get_prompt(text):
+    prompt = (
+        "Проанализируй текст и скажи, является ли это вакансией/проектом с поиском разработчиков. Ответ дай в "
+        'формате JSON СТРОГО по шаблону:{"vacancy": true/false} и никаких лишних символов.'
+        f"Текст: {text}"
+    )
+    return prompt
+
+
 @client.on(events.NewMessage(chats=all_chats))
 async def my_event_handler(event):
     msg = event.text
-    regex_pattern = re.compile('|'.join(regex_bid), re.IGNORECASE)
-    if regex_pattern.search(msg):
-        log.info(f"Сообщение с канала {event.chat_id} пересылаю в {my_chat}!")
+    ai_client = Client()
+    log.info(f"Получено сообщение с канала {event.chat_id}. Начинаю обработку!")
+    response = ai_client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system",
+             "content": "Ты продвинутый анализатор текста"
+             },
+            {"role": "user",
+             "content": get_prompt(msg)}
+        ]
+    )
+    result = response.choices[0].message.content
+    result_dict = await json_to_dict(json_message=result)
+    if result_dict['vacancy']:
+        log.info(f"Сообщение с канала {event.chat_id} отправляю в CMS!")
         response = await send_vacancy(msg, str(event.chat_id))
         if response:
             pass
